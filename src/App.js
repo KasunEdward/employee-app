4bdba6e7b868664eb3c8d78a9aa840678d8f1aab
Kasun Edward <kasunedward48@gmail.com>
2022-07-09 23:22:25 +0530

    Set app default path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import EmployeeList from './feature/Employee';
+import "./App.css";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import EmployeeList from "./feature/Employee";
 
 function App() {
   return (
-    <EmployeeList/>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* set app default path to /employee/list */}
+        <Route path="/" element={<Navigate to="/employee" />} />
+        <Route path="/employee" element={<Navigate to="/employee/list" />} />
+        <Route path="/employee/list" element={<EmployeeList />} />
+        {/* set path not listed to /employee/list */}
+        <Route path="*" element={<Navigate to="/employee" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
