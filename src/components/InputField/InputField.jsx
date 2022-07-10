279423d21ca76f951f3d059c80854c284b91f5e6
Kasun Edward <kasunedward48@gmail.com>
2022-07-10 22:08:22 +0530

    Add InputField component

---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TextField from '@mui/material/TextField';
+
+export const InputField = (props) => {
+    console.log(props);
+  return (
+    <TextField
+    id="outlined-basic"
+    {...(props.register && props.register(props.name))}
+    name={props.name}
+    label={props.label}
+    error={props.error}
+    helperText={props.errorMessage}
+    // defaultValue="Hello World"
+  />
+  );
+};
--- src/features/Employee/AddEmployeeForm.jsx
+++ src/features/Employee/AddEmployeeForm.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import CustomButton from "../../components/CustomButton";
+import InputField from "../../components/InputField";
+import { TextField } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { AddEmployee } from "../../services/employeeService";
 
 const SgPhoneRegex = /\+65(6|8|9)\d{7}/g;
 
 //form validation schema
-const schema = yup
-  .object().shape({
-    firstName: yup.string().min(6).max(10).required(),
-    lastName: yup.string().min(6).max(10).required(),
-    email: yup.string().email().required(),
-    phone: yup.string().matches(SgPhoneRegex, "Invalid phone number").required(),
-    gender: yup.string().oneOf(["Male", "Female"]).required(),
-  })
+const schema = yup.object().shape({
+  firstName: yup.string().min(6).max(10).required(),
+  lastName: yup.string().min(6).max(10).required(),
+  email: yup.string().email().required(),
+  phone: yup.string().matches(SgPhoneRegex, "Invalid phone number").required(),
+  gender: yup.string().oneOf(["Male", "Female"]).required(),
+});
 
 const AddEmployeeForm = () => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
@@ -31,12 +33,33 @@ const AddEmployeeForm = () => {
 
   const onSubmit = (data) => {
     dispatch(AddEmployee(data));
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <p>{errors.firstName?.message}</p>
+      {/* <Controller
+      name="firstName"
+      control={control}
+      render={({ field }) => <InputField label={"firstName"} value={field.value} error={!!errors.firstName} errorMessage={errors?.firstName?.message}{...field} />}
+      /> */}
+      <InputField
+        name="firstName"
+        label="firstName"
+        error={Boolean(errors.firstName)}
+        errorMessage={errors.firstName?.message}
+        register = {register}
+      />
+{/* 
+      <TextField 
+          id="outlined-basic" 
+          name="firstName" 
+          label="First Name" 
+          variant="outlined" 
+          fullWidth 
+          {...register("firstName")}
+          error={Boolean(errors.firstName)}
+          helperText={errors.firstName?.message}
+        /> */}
 
       <input {...register("lastName")} />
       <p>{errors.lastName?.message}</p>
