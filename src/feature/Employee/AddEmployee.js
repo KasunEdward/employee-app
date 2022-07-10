66bd938e962bf89118dd436e1b6614937cb16555
Kasun Edward <kasunedward48@gmail.com>
2022-07-10 11:41:08 +0530

    Load Emlpyee grid using action data

---
+++ src/feature/Employee/AddEmployee.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddEmployee = () => {
+    return(
+        <h1>Add</h1>
+    )
+}
+
+export default AddEmployee;
--- src/feature/Employee/EmployeeList.js
+++ src/feature/Employee/EmployeeList.js
@@ -1,26 +1,47 @@
-import React, {useState} from 'react';
-import { AgGridReact } from 'ag-grid-react';
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { EmployeeActions } from "./actions";
 
 const EmployeeList = () => {
-    const [rowData] = useState([
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxster", price: 72000}
-    ]);
-    
-    const [columnDefs] = useState([
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price' }
-    ])
-    return(
-        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+  const [rowData, setRowData] = useState([]);
+  const columnDefs = [
+    { field: "uuid", hide: true },
+    { field: "firstName" },
+    { field: "lastName" },
+    { field: "email" },
+    { field: "phone" },
+    { field: "gender" },
+  ];
+  useEffect(() => {
+    EmployeeActions.getEmployee().then(
+      (response) => {
+        setRowData(response);
+      },
+      (error) => {
+        console.log("ghghgh");
+      }
+    );
+  }, []);
+
+  let navigate = useNavigate();
+
+  const handleClickAdd = (event) => {
+    event.preventDefault();
+    navigate("../employee/add");
+  };
+  return (
+    <>
+      <Button onClick={handleClickAdd}>Add</Button>
+      <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
         <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
       </div>
-    )
-}
+    </>
+  );
+};
 
 export default EmployeeList;
