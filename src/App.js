da2be1dd5deab5a35d572ba9dcaa5c78fef04883
Kasun Edward <kasunedward48@gmail.com>
2022-07-10 11:40:36 +0530

    Add employee/add rule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import EmployeeList from "./feature/Employee";
+import AddEmployee from "./feature/Employee/AddEmployee";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Navigate to="/employee" />} />
         <Route path="/employee" element={<Navigate to="/employee/list" />} />
         <Route path="/employee/list" element={<EmployeeList />} />
+        <Route path="/employee/add" element={<AddEmployee />} />
         {/* set path not listed to /employee/list */}
         <Route path="*" element={<Navigate to="/employee" />} />
       </Routes>
