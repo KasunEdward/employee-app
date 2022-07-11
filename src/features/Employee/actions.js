70defd27765720e134e531c1bffa0cccb39f0ec2
Kasun Edward <kasunedward48@gmail.com>
2022-07-11 11:45:20 +0530

    remove actions.js

--- src/features/Employee/actions.js
+++
@@ -1,7 +0,0 @@
-export const EmployeeActions = {
-    // get employees arrray
-    getEmployee: async() => {
-        const employeeList = await localStorage.getItem('employeeList');
-        return JSON.parse(employeeList);
-    }
-}
